a53e6823bb5c2c1e4550733507b618a09509fbb3
PARESH SUTHAR <129194145+pareshsutharr@users.noreply.github.com>
2024-11-08 15:20:45 +0530

    Update firebase.js

--- project-5/src/config/firebase.js
+++ project-5/src/config/firebase.js
@@ -6,12 +6,12 @@ import {getFirestore}  from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCN7HCI__Z8CYZgoOBZ7ywpoCgehSCCG5E",
-  authDomain: "vite-contact-59d8e.firebaseapp.com",
-  projectId: "vite-contact-59d8e",
-  storageBucket: "vite-contact-59d8e.firebasestorage.app",
-  messagingSenderId: "375431928770",
-  appId: "1:375431928770:web:e6751bf47ab9bb2e1b545b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
